0b5bff67276e95919add91c3e04222c78bfcc142
SAMSUDDIN ANSARI <biosamsuddin@gmail.com>
2025-06-06 00:14:32 +0530

    status commented out

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
-from .routes.video import bp as video_bp
-from .routes.story import bp as stories_bp
-from .routes.dp import bp as dp_bp
-from .routes.status import bp as status_bp
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    app.register_blueprint(video_bp)
-    app.register_blueprint(stories_bp)
-    app.register_blueprint(dp_bp)
-    app.register_blueprint(status_bp)
+    from .routes import video, story, dp
+    app.register_blueprint(video.bp)
+    app.register_blueprint(story.bp)
+    app.register_blueprint(dp.bp)
 
     return app
